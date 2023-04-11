b203ef0e4a6d7c7f14b84dc529530eb3c46dc194
Daniel Walls <walls.dk@gmail.com>
2023-04-11 10:58:06 -0700

    dictionary stuff

---
+++ Core/Dictionary/DictionaryGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace poetools.Core.Dictionary
+{
+    /// <summary>
+    /// A inspector-friendly generator for dictionaries.
+    /// </summary>
+    /// <typeparam name="TKey">The key type.</typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
+    [Serializable]
+    public class DictionaryGenerator<TKey, TValue> :
+        DictionaryGeneratorAdvanced<TKey, TValue, SerializeFieldPair<TKey, TValue>>
+    {
+    }
+}
---
+++ Core/Dictionary/SceneValuePair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TriInspector;
+using UnityEngine;
+
+namespace poetools.Core.Dictionary
+{
+    /// <summary>
+    /// A pair where a key maps to a Unity scene. The value will be the string scene name,
+    /// and is decorated with a custom TriInspector property drawer.
+    /// </summary>
+    /// <typeparam name="TKey">The type of key.</typeparam>
+    [Serializable]
+    public class SceneValuePair<TKey> : SerializedKeyValuePair<TKey, string>
+    {
+        [SerializeField]
+        private TKey key;
+
+        [Scene]
+        [SerializeField]
+        private string scene;
+
+        /// <inheritdoc/>
+        public override TKey Key => key;
+
+        /// <inheritdoc/>
+        public override string Value => scene;
+    }
+}
