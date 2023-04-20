16c4a9897b38e37c35bbc47da1bc9b0be680f8ed
Daniel Walls <walls.dk@gmail.com>
2023-04-19 17:11:22 -0700

    added UI prefabs

---
+++ Integrations/ColorBlockApplier.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Integrations
+{
+    public class ColorBlockApplier : MonoBehaviour
+    {
+        public ColorBlockAuthoring authoring;
+        public Selectable[] targets;
+
+        private void UpdateTargets()
+        {
+            foreach (var target in targets)
+                target.colors = authoring.ColorBlock;
+        }
+
+        private void Awake()
+        {
+            UpdateTargets();
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            UpdateTargets();
+        }
+#endif
+    }
+}
