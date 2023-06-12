041540aaa7b17fe71e3af5b3bf58b13c4123f5d0
Daniel Walls <walls.dk@gmail.com>
2023-06-11 23:30:49 -0700

    cool interface reference things

---
+++ Core/InterfaceReference.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using TriInspector;
+using UnityEngine;
+
+namespace poetools.Core
+{
+    [Serializable]
+    [DeclareHorizontalGroup("group")]
+    public class InterfaceReference<T> where T : class
+    {
+        [Flags]
+        public enum SearchType
+        {
+            Root = 1 << 0,
+            Children = 1 << 1,
+            Parent = 1 << 2,
+        }
+
+        [Group("group")]
+        [ValidateInput(nameof(ValidateTarget))]
+        [SerializeField]
+        private GameObject target; // todo: require type of interface
+
+        private TriValidationResult ValidateTarget()
+        {
+            if (target != null && target.TryGetComponent(out T _)) return TriValidationResult.Error($"Target does not contain {typeof(T).Name}");
+            return TriValidationResult.Valid;
+        }
+
+        [Group("group")]
+        [EnumToggleButtons]
+        [SerializeField]
+        private SearchType type;
+
+        public T ResolveSingle()
+        {
+            T result = null;
+
+            if (type.HasFlag(SearchType.Root))
+                result = target.GetComponent<T>();
+
+            if (result != null && type.HasFlag(SearchType.Children))
+                result = target.GetComponentInChildren<T>();
+
+            if (result != null && type.HasFlag(SearchType.Parent))
+                result = target.GetComponentInParent<T>();
+
+            return result;
+        }
+
+        public T[] ResolveMultiple()
+        {
+            T[] result = null;
+
+            if (type.HasFlag(SearchType.Root))
+                result = target.GetComponents<T>();
+
+            if (result != null && type.HasFlag(SearchType.Children))
+                result = target.GetComponentsInChildren<T>();
+
+            if (result != null && type.HasFlag(SearchType.Parent))
+                result = target.GetComponentsInParent<T>();
+
+            return result;
+        }
+    }
+}
